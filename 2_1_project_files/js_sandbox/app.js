3fa98bc50d8fa30eb3d5ef96ecd334a529d9ff50
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-12 12:53:10 -0500

    worked on different console outputs

---
+++ 2_1_project_files/js_sandbox/app.js
@@ -0,0 +1,27 @@
+/** 
+ * Log to console 
+ * console.log('Hello World')
+ */
+
+console.log('Hello World');
+console.log(123);
+console.log(true);
+console.log([1,2,3,4]);
+console.log(greeting);
+console.log({a:1,b:2});
+console.log({a:1,b:2});
+console.log();
+console.log();
+console.error('This is an error');
+
+console.warn('This is a warning');
+
+console.time('Hello');
+console.time('Hello');
+console.time('Hello');
+console.time('Hello');
+console.time('Hello');
+console.time('Hello');
+console.time('Hello');
+console.time('Hello');
+console.timeend('Hello')
