78e749d2157a594a758979e6f4cefece139d0326
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-16 23:50:22 -0500

    added error image for function expression lesson

--- 2_17_project_files/js_sandbox/app.js
+++ 2_17_project_files/js_sandbox/app.js
@@ -1,27 +1,51 @@
-// Global Scope
-var a = 1;
-let b = 2;
-const c = 3;
+// // Global Scope
+// var a = 1;
+// let b = 2;
+// const c = 3;
 
-// function test() {
-//   var a = 4;
-//   let b = 5;
-//   const c = 6;
-//   console.log('Function Scope: ', a, b, c);
+// // function test() {
+// //   var a = 4;
+// //   let b = 5;
+// //   const c = 6;
+// //   console.log('Function Scope: ', a, b, c);
+// // }
+
+// // test();
+
+// // if(true) {
+// //   // Block Scope
+// //   var a = 4;
+// //   let b = 5;
+// //   const c = 6;
+// //   console.log('If Scope: ', a, b, c);
+// // }
+
+// // for(var a = 0; a < 10; a++) {
+// //   console.log(`Loop: ${a}`);
+// // }
+
+// console.log('Global Scope: ', a, b, c);
+
+// function greet(firstName, lastName){
+//     // console.log('Hello ');//all we have done is delcared the function you usually don't log a function unless you want to see if it works 
+//     return 'Hello' + firstName; //you usually return it 
 // }
+// console.log (greet());//calling the function without arguments, you will get undefined because firstName and lastName is not defined 
 
-// test();
+//adding default parameters so that it does not return undefined 
 
-// if(true) {
-//   // Block Scope
-//   var a = 4;
-//   let b = 5;
-//   const c = 6;
-//   console.log('If Scope: ', a, b, c);
-// }
+// function greet(firstName = 'Ramona', lastName = 'Saintandre'){
+//     return 'Hello ' + firstName;
+// }console.log (greet());
+ 
 
-// for(var a = 0; a < 10; a++) {
-//   console.log(`Loop: ${a}`);
-// }
+//function expressions is setting a function as a variable assignment
 
-console.log('Global Scope: ', a, b, c);
+const square = function(x = 3){//you can set a default for the function, for when you call it 
+return x*x;
+};
+console.log(square(4));//not sure why this is not working right I keep getting 16 instead of 12 
+
+
+
+
