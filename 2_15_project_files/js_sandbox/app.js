acee150936c110f37777eeb9863825418567744a
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-17 00:18:06 -0500

    finished forLoop, learned continue

--- 2_15_project_files/js_sandbox/app.js
+++ 2_15_project_files/js_sandbox/app.js
@@ -1,74 +1,91 @@
-// FOR LOOP
+// // FOR LOOP
 
-// for(let i = 0; i < 10; i++){
-//   if(i === 2){
-//     console.log('2 is my favorite number');
-//     continue;
-//   }
+// // for(let i = 0; i < 10; i++){
+// //   if(i === 2){
+// //     console.log('2 is my favorite number');
+// //     continue;
+// //   }
 
-//   if(i === 5){
-//     console.log('Stop the loop');
-//     break;
-//   }
+// //   if(i === 5){
+// //     console.log('Stop the loop');
+// //     break;
+// //   }
 
-//   console.log('Number '+ i);
+// //   console.log('Number '+ i);
+// // }
+
+// // WHILE LOOP
+
+// // let i = 0;
+
+// // while(i < 10){
+// //   console.log('Number ' + i);
+// //   i++;
+// // }
+
+// // DO WHILE
+
+// // let i = 100;
+
+// // do {
+// //   console.log('Number ' + i);
+// //   i++;
+// // }
+
+// // while(i < 10);
+
+// // LOOP THROUGH ARRAY
+// const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+// // for(let i = 0; i < cars.length; i++){
+// //   console.log(cars[i]);
+// // }
+
+// // FOREACH
+// // cars.forEach(function(car, index, array){
+// //   console.log(`${index} : ${car}`);
+// //   console.log(array);
+// // });
+
+// // MAP
+// // const users  = [
+// //   {id: 1, name:'John'},
+// //   {id: 2, name: 'Sara'},
+// //   {id: 3, name: 'Karen'},
+// //   {id: 4, name: 'Steve'}
+// // ];
+
+// // const ids = users.map(function(user){
+// //   return user.id;
+// // });
+
+// // console.log(ids);
+
+
+// // FOR IN LOOP
+// const user = {
+//   firstName: 'John', 
+//   lastName: 'Doe',
+//   age: 40
 // }
 
-// WHILE LOOP
-
-// let i = 0;
-
-// while(i < 10){
-//   console.log('Number ' + i);
-//   i++;
+// for(let x in user){
+//   console.log(`${x} : ${user[x]}`);
 // }
 
-// DO WHILE
-
-// let i = 100;
-
-// do {
-//   console.log('Number ' + i);
-//   i++;
+// For LOOPS
+// You alway want to use var or let inside of a loop, you can not use const, because it is always changing 
+//
+// for (let i = 0; i <= 10; i++ ){
+// // console.log('Number '+ i);
+// if (i ===2){
+//   console.log('2 is my fav number');
+//   continue;
+// }
+// if(i = 5){
+//   console.log('Stop!!')
+// break;
 // }
 
-// while(i < 10);
-
-// LOOP THROUGH ARRAY
-const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
-
-// for(let i = 0; i < cars.length; i++){
-//   console.log(cars[i]);
+// console.log('number '+ i);
 // }
-
-// FOREACH
-// cars.forEach(function(car, index, array){
-//   console.log(`${index} : ${car}`);
-//   console.log(array);
-// });
-
-// MAP
-// const users  = [
-//   {id: 1, name:'John'},
-//   {id: 2, name: 'Sara'},
-//   {id: 3, name: 'Karen'},
-//   {id: 4, name: 'Steve'}
-// ];
-
-// const ids = users.map(function(user){
-//   return user.id;
-// });
-
-// console.log(ids);
-
-
-// FOR IN LOOP
-const user = {
-  firstName: 'John', 
-  lastName: 'Doe',
-  age: 40
-}
-
-for(let x in user){
-  console.log(`${x} : ${user[x]}`);
-}
