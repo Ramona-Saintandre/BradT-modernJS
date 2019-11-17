4735275517c0e2a2bed1c7bf33941f878fd915aa
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-17 08:30:40 -0500

    went over windows properties and objects

--- 2_16_project_files/js_sandbox/app.js
+++ 2_16_project_files/js_sandbox/app.js
@@ -1,59 +1,80 @@
-// WINDOW METHODS / OBJECTS / PROPERTIES
+// // WINDOW METHODS / OBJECTS / PROPERTIES
 
-// Alert
-//alert('Hello World');
+// // Alert
+// //alert('Hello World');
 
-// Prompt
-// const input = prompt();
+// // Prompt
+// // const input = prompt();
+// // alert(input);
+
+// // Confirm
+// // if(confirm('Are you sure')){
+// //   console.log('YES');
+// // } else {
+// //   console.log('NO');
+// // }
+
+// let val;
+
+// // Outter height and width
+// val = window.outerHeight;
+// val = window.outerWidth;
+
+// // Inner height and width
+// val = window.innerHeight;
+// val = window.innerWidth;
+
+// // Scroll points
+// val = window.scrollY;
+// val = window.scrollX;
+
+// // Location Object
+// val = window.location;
+// val = window.location.hostname;
+// val = window.location.port;
+// val = window.location.href;
+// val = window.location.search;
+
+// // Redirect
+// //window.location.href = 'http://google.com';
+// //Reload
+// //window.location.reload();
+
+// // History Object
+
+// // window.history.go(-2);
+// // val = window.history.length;
+
+// // Navigator Object
+// val = window.navigator;
+// val = window.navigator.appName;
+// val = window.navigator.appVersion;
+// val = window.navigator.userAgent;
+// val = window.navigator.platform;
+// val = window.navigator.vendor;
+// val = window.navigator.language;
+
+
+
+// console.log(val);
+
+// window.alert('Hello World');
+
+// const input =prompt();
 // alert(input);
 
-// Confirm
-// if(confirm('Are you sure')){
-//   console.log('YES');
-// } else {
-//   console.log('NO');
+// if (confirm('are you sure')){
+//     console.log('yes');    
+// }else{
+//     console.log('No');
 // }
+//Height and width of a window
 
 let val;
-
-// Outter height and width
+//outer height
 val = window.outerHeight;
-val = window.outerWidth;
 
-// Inner height and width
-val = window.innerHeight;
+//outer width
 val = window.innerWidth;
-
-// Scroll points
-val = window.scrollY;
-val = window.scrollX;
-
-// Location Object
-val = window.location;
-val = window.location.hostname;
-val = window.location.port;
-val = window.location.href;
-val = window.location.search;
-
-// Redirect
-//window.location.href = 'http://google.com';
-//Reload
-//window.location.reload();
-
-// History Object
-
-// window.history.go(-2);
-// val = window.history.length;
-
-// Navigator Object
-val = window.navigator;
-val = window.navigator.appName;
-val = window.navigator.appVersion;
-val = window.navigator.userAgent;
-val = window.navigator.platform;
-val = window.navigator.vendor;
-val = window.navigator.language;
-
-
 
 console.log(val);
