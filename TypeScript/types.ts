4b341cb96906272b6f93a98d63c0bf5c8e31f689
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-06 21:26:34 -0500

    finished types

--- TypeScript/types.ts
+++ TypeScript/types.ts
@@ -2,12 +2,21 @@ let myString :string;
 let myNum: number;
 let myBoo1:boolean;
 let myVar:any;
+let strArr :string[];
+let booArr:boolean[];
 
 myString = "Hello World" + " My name is Ramona";
 myNum =5+5;
 myBoo1 = false;
-mVar = 5;
-
+myVar = 5;
+strArr = ['hello','world','my','name','is','Ramona']
+let boo1Arr:Array<boolean>;
+let strNumTuple:[string,number];
 console.log(myBoo1);
 console.log(myString);
 console.log(myVar);
+console.log(strArr);
+boo1Arr = [true,false,true];
+
+strNumTuple = ['Hello',4]
+console.log(strNumTuple);
