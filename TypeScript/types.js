c3bd8d6e750c790109dd119afbc4b06c0a34e4d3
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-06 21:20:06 -0500

    worked with different types in TS

--- TypeScript/types.js
+++ TypeScript/types.js
@@ -1,1 +1,11 @@
-console.log('Hello from ts');
+var myString;
+var myNum;
+var myBoo1;
+var myVar;
+myString = "Hello World" + " My name is Ramona";
+myNum = 5 + 5;
+myBoo1 = false;
+mVar = 5;
+console.log(myBoo1);
+console.log(myString);
+console.log(myVar);
--- TypeScript/types.ts
+++ TypeScript/types.ts
@@ -1,1 +1,13 @@
-console.log('Hello from ts');
+let myString :string;
+let myNum: number;
+let myBoo1:boolean;
+let myVar:any;
+
+myString = "Hello World" + " My name is Ramona";
+myNum =5+5;
+myBoo1 = false;
+mVar = 5;
+
+console.log(myBoo1);
+console.log(myString);
+console.log(myVar);
